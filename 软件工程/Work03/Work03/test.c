f4b4ea55965727e99b263329750001c3dd96acb4
KexinAnswer <925433202@qq.com>
2018-12-12 23:34:33 +0800

    test

---
+++ 软件工程/Work03/Work03/test.c
@@ -0,0 +1,47 @@
+//在屏幕上打印杨辉三角。
+//1
+//1 1
+//1 2 1
+//1 3 3 1
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//void Tringle(int n)
+//{
+//	int tringle[6] = { 1 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 6; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			
+//		}
+//	}
+//
+//}
+
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int n = 0;
+	int arr1[100] = {0};
+	int arr2[100] = { 0 };
+	
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (j = 1)
+		}
+
+	}
+
+
+
+	system("pause");
+	return 0;
+}
